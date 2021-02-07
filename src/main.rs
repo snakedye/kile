58d32d8a1fd9449cba47a511da52043582d6c6d0
Bryan Ndjeutcha <ndjeutcha@gmail.com>
2021-02-07 09:41:17 -0500

    combi layout generator

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
 use std::env;
 mod basic;
 mod layout;
+mod combi;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect(); // arguments vector
+    let mut layouts: Vec<&str> = Vec::new(); // layout vector
 
-    if args.len() < 7 {
-        println!("Not enough arguments");
-        std::process::exit(0);
+    let mut client_count:u32=0;
+    let mut master_count:u32=0;
+    let mut master_width_factor:f32=0.5;
+    let mut screen_width:u32=0;
+    let mut screen_height:u32=0;
+
+    for i in 1..args.len() {
+        if i <= args.len()-6 {
+            // println!("layout: {}", args[i]);
+            layouts.push(&args[i]);
+        } else {
+            let index=i-layouts.len()-1;
+            match index {
+                0usize=>client_count=args[i].parse().unwrap(),
+                1usize=>master_count=args[i].parse().unwrap(),
+                2usize=>master_width_factor=args[i].parse().unwrap(),
+                3usize=>screen_width=args[i].parse().unwrap(),
+                4usize=>screen_height=args[i].parse().unwrap(),
+                _ => {
+                    println!("Too much arguments");
+                    std::process::exit(0);
+                }
+            }
+        }
     }
-
-    let layout:&str=&args[1];
-    let client_count:u32=args[2].parse().unwrap();
-    let master_count:u32=args[3].parse().unwrap();
-    let master_width_factor:f32=args[4].parse().unwrap();
-    let screen_width:u32=args[5].parse().unwrap();
-    let screen_height:u32=args[6].parse().unwrap();
 
     let mut window_tree:Vec<basic::Frame>=Vec::new();
 
     let output:basic::Frame=basic::new_frame(0,0,screen_width,screen_height);
 
-    match layout {
-        "left" => window_tree=layout::master_stack(window_tree,output, client_count, master_count, master_width_factor),
-        "center" => window_tree=layout::centered_master(window_tree,output, client_count, master_count, master_width_factor),
-        "magic" => window_tree=layout::magic_master(window_tree,output, client_count, master_count, master_width_factor),
-        "nested" => window_tree=layout::nested(window_tree,output, client_count, master_count, master_width_factor),
-        _ => {
-            println!("{} isn't a valid layout", layout);
-            std::process::exit(0);
-        }
+    if layouts.len() > 1 {
+        window_tree=combi::combi(layouts, window_tree, output, client_count, master_count, master_width_factor);
+    } else {
+        window_tree=combi::chosen_layout(layouts[0], window_tree, output, client_count, master_count, master_width_factor);
     }
 
     for window in window_tree {
         println!("{} {} {} {}", window.x, window.y, window.w, window.h);
     }
 }
-
